91cd9e9715c8700faae67f93ba0492bf937e14a7
hussain18 <talib.hussain18@vit.edu>
2021-06-22 11:23:57 +0530

    first commit

---
+++ front_end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+function App() {
+  return (
+    <div className="App container">
+      Hi FindMeHome!!!
+    </div>
+  );
+}
+
+export default App;
